cb217ee67f11972f7ef74cc4ae6d0d1be3179b44
Skeep <Skeep747@gmail.com>
2018-08-29 10:30:03 +0300

    рефакторинг

--- WpfParser/MainWindow.xaml.cs
+++ WpfParser/MainWindow.xaml.cs
@@ -9,24 +9,24 @@ namespace WpfParser
     public partial class MainWindow
     {
         private readonly Parser _parser = new Parser();
-
         private readonly SaveFileDialog _dlg = new SaveFileDialog();
+        private readonly ObservableCollection<LogItem> _dataItemsLog = new ObservableCollection<LogItem>();
 
         public MainWindow()
         {
             InitializeComponent();
-            var dataItemsLog = new ObservableCollection<LogItem>();
-            DataGridLog.ItemsSource = dataItemsLog;
-            _parser.OnLogResult += async result =>
-            {
-                await Application.Current.Dispatcher.BeginInvoke(
-                    new Action(() => dataItemsLog.Insert(0, result)));
-                if (result.Result != "Все страницы просканированы!") return;
-                UrlTextBox.IsEnabled = true;
-                StartParseButton.IsEnabled = true;
-                AbortButton.IsEnabled = false;
-                _parser.EndPage = 1;
-            };
+            DataGridLog.ItemsSource = _dataItemsLog;
+            _parser.OnLogResult += OnLogResult;
+        }
+
+        private async void OnLogResult(LogItem logItem)
+        {
+            await Application.Current.Dispatcher.BeginInvoke(new Action(() => _dataItemsLog.Insert(0, logItem)));
+            if (logItem.Result != "Все страницы просканированы!") return;
+            UrlTextBox.IsEnabled = true;
+            StartParseButton.IsEnabled = true;
+            AbortButton.IsEnabled = false;
+            _parser.EndPage = 1;
         }
 
         private void StartParseButton_OnClick(object sender, RoutedEventArgs e)
@@ -35,7 +35,6 @@ namespace WpfParser
             {
                 _parser.ParseUrl = new UriBuilder(UrlTextBox.Text).Uri.AbsoluteUri;
                 if (!UrlTextBox.Text.Contains("prom.ua")) throw new Exception("Парсер предназначен только для сайта Prom.ua");
-                _parser.ParsePages();
                 _dlg.FileName = _parser.ParseUrl.Substring(_parser.ParseUrl.IndexOf("prom.ua/", StringComparison.Ordinal) + 8, 
                     _parser.ParseUrl.Length - _parser.ParseUrl.IndexOf("prom.ua/", StringComparison.Ordinal) - 8);
                 if (TxtRadioButton.IsChecked != null && (bool) TxtRadioButton.IsChecked)
--- WpfParser/Parser.cs
+++ WpfParser/Parser.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
 using NPOI.SS.UserModel;
 using NPOI.XSSF.UserModel;
 
@@ -18,6 +17,7 @@ namespace WpfParser
         public bool Abort { private get; set; }
 
         private readonly HttpClient _client = new HttpClient();
+        private readonly HtmlDocument _htmlDocument = new HtmlDocument();
         private FileStream _writer;
         private XSSFWorkbook _xssf;
         private ISheet _sheet;
@@ -25,27 +25,22 @@ namespace WpfParser
 
         public event Action<LogItem> OnLogResult;
 
-        public async void ParsePages()
+        public async void StartParse()
         {
             try
             {
-                var html = await _client.GetStringAsync(ParseUrl);
-                var htmlDocument = new HtmlDocument();
-                htmlDocument.LoadHtml(html);
-                var pagesCount = htmlDocument
-                    .DocumentNode.Descendants("a")
-                    .Where(node => node.GetAttributeValue("class", "")
-                        .Equals("x-pager__item")).ToArray();
-                EndPage = pagesCount.Length != 0 ? int.Parse(pagesCount[pagesCount.Length - 1].InnerText) : 0;
+                _htmlDocument.LoadHtml(await _client.GetStringAsync(ParseUrl));
             }
             catch (Exception exception)
             {
                 OnLogResult?.Invoke(new LogItem { Status = "Error", Result = exception.Message });
+                return;
             }
-        }
-
-        public async void StartParse()
-        {
+            var pagesCount = _htmlDocument
+                .DocumentNode.Descendants("a")
+                .Where(node => node.GetAttributeValue("class", "")
+                    .Equals("x-pager__item")).ToArray();
+            EndPage = pagesCount.Length != 0 ? int.Parse(pagesCount[pagesCount.Length - 1].InnerText) : 1;
             using (_writer = File.Create(FileName))
             {
                 if (FileName.Contains("xlsx"))
@@ -57,7 +52,7 @@ namespace WpfParser
                     row.CreateCell(0).SetCellValue("Name");
                     row.CreateCell(1).SetCellValue("Availability");
                     row.CreateCell(2).SetCellValue("Price");
-                    //row.CreateCell(3).SetCellValue("Code");
+                    row.CreateCell(3).SetCellValue("Code");
                 }
                 for (var i = 1; i <= EndPage; i++)
                 {
@@ -69,10 +64,75 @@ namespace WpfParser
                         EndPage = 1;
                         return;
                     }
-                    var html = await GetSourceByPage(i);
-                    var htmlDocument = new HtmlDocument();
-                    htmlDocument.LoadHtml(html);
-                    Parse(htmlDocument);
+                    _htmlDocument.LoadHtml(await _client.GetStringAsync(ParseUrl + ";" + i));
+                    var pageUrls = _htmlDocument
+                        .DocumentNode.Descendants("div")
+                        .Where(node => node.GetAttributeValue("class", "")
+                            .Equals("x-gallery-tile__content")).ToArray();
+
+                    foreach (var pageUrl in pageUrls)
+                    {
+                        var itemUrl = pageUrl
+                            .Descendants("a").FirstOrDefault(node => node.GetAttributeValue("class", "")
+                                .Equals("x-gallery-tile__name"))?.GetAttributeValue("href", "");
+                        _htmlDocument.LoadHtml(await _client.GetStringAsync(itemUrl));
+                        var items = _htmlDocument
+                            .DocumentNode.Descendants("div")
+                            .Where(node => node.GetAttributeValue("class", "")
+                                .Equals("x-product-info__content")).ToArray();
+                        if (!FileName.Contains("xlsx"))
+                        {
+                            foreach (var item in items)
+                            {
+                                var text = item
+                                               .Descendants("h1").FirstOrDefault(node => node
+                                                   .GetAttributeValue("class", "")
+                                                   .Equals("x-title"))?.InnerText.Trim() + '\t' +
+                                           item
+                                               .Descendants("div").FirstOrDefault(node => node
+                                                   .GetAttributeValue("class", "")
+                                                   .Contains("x-product-presence"))?.InnerText.Trim() + '\t' +
+                                           item
+                                               .Descendants("div").FirstOrDefault(node => node
+                                                   .GetAttributeValue("class", "")
+                                                   .Contains("x-product-price__value"))?.InnerText.Trim() + '\t' +
+                                           item
+                                               .Descendants("div").FirstOrDefault(node => node
+                                                   .GetAttributeValue("class", "")
+                                                   .Contains("x-product-info__identity-item"))?.InnerText.Trim() + '\n';
+                                var info = new UTF8Encoding(true).GetBytes(text);
+                                _writer?.Write(info, 0, info.Length);
+                            }
+                        }
+                        else
+                        {
+                            foreach (var item in items)
+                            {
+                                _indexRow++;
+                                var row = _sheet.CreateRow(_indexRow);
+                                row.CreateCell(0)
+                                    .SetCellValue(item
+                                        .Descendants("h1").FirstOrDefault(node => node
+                                            .GetAttributeValue("class", "")
+                                            .Equals("x-title"))?.InnerText.Trim());
+                                row.CreateCell(1)
+                                    .SetCellValue(item
+                                        .Descendants("div").FirstOrDefault(node => node
+                                            .GetAttributeValue("class", "")
+                                            .Contains("x-product-presence"))?.InnerText.Trim());
+                                row.CreateCell(2)
+                                    .SetCellValue(item
+                                        .Descendants("div").FirstOrDefault(node => node
+                                            .GetAttributeValue("class", "")
+                                            .Contains("x-product-price__value"))?.InnerText.Trim());
+                                row.CreateCell(3)
+                                    .SetCellValue(item
+                                        .Descendants("div").FirstOrDefault(node => node
+                                            .GetAttributeValue("class", "")
+                                            .Contains("x-product-info__identity-item"))?.InnerText.Trim());
+                            }
+                        }
+                    }
                     OnLogResult?.Invoke(new LogItem { Status = "OK", Result = $"Готова страница № {i} из {EndPage}" });
                 }
                 if(FileName.Contains("xlsx"))
@@ -81,63 +141,12 @@ namespace WpfParser
             OnLogResult?.Invoke(new LogItem { Status = "OK", Result = "Все страницы просканированы!"});
         }
 
-        private async Task<string> GetSourceByPage(int page)
-        {
-            var currentUrl = ParseUrl + ";" + page;
-            var html = await _client.GetStringAsync(currentUrl);
-            return html;
-        }
 
-        private void Parse(HtmlDocument htmlDocument)
-        {
-            var items = htmlDocument
-                .DocumentNode.Descendants("div")
-                .Where(node => node.GetAttributeValue("class", "")
-                    .Equals("x-gallery-tile__content")).ToArray();
-            if (!FileName.Contains("xlsx"))
-            {
-                foreach (var item in items)
-                {
-                    var text = item
-                                   .Descendants("a").FirstOrDefault(node => node
-                                       .GetAttributeValue("class", "")
-                                       .Equals("x-gallery-tile__name"))?.InnerText.Trim() + "\t" +
-                               item
-                                   .Descendants("div").FirstOrDefault(node => node
-                                       .GetAttributeValue("class", "")
-                                       .Contains("x-product-presence"))?.InnerText.Trim() + "\t" +
-                               item
-                                   .Descendants("div").FirstOrDefault(node => node
-                                       .GetAttributeValue("class", "")
-                                       .Contains("x-gallery-tile__price"))?.InnerText.Trim() + "\n";
-                    var info = new UTF8Encoding(true).GetBytes(text);
-                    _writer?.Write(info, 0, info.Length);
-                }
-            }
-            else
-            {
-                foreach (var item in items)
-                {
-                    _indexRow++;
-                    var row = _sheet.CreateRow(_indexRow);
-                    row.CreateCell(0)
-                        .SetCellValue(item
-                            .Descendants("a").FirstOrDefault(node => node
-                                .GetAttributeValue("class", "")
-                                .Equals("x-gallery-tile__name"))?.InnerText.Trim());
-                    row.CreateCell(1)
-                        .SetCellValue(item
-                            .Descendants("div").FirstOrDefault(node => node
-                                .GetAttributeValue("class", "")
-                                .Contains("x-product-presence"))?.InnerText.Trim());
-                    row.CreateCell(2)
-                        .SetCellValue(item
-                            .Descendants("div").FirstOrDefault(node => node
-                                .GetAttributeValue("class", "")
-                                .Contains("x-gallery-tile__price"))?.InnerText.Trim());
-                }
-            }
-        }
+
+
+
+
+
 
         public void RaiseOnResult(string status, string result)
         {
